09f79eef8d45498fb41cab292b50e2ff7363922b
Nat! <nat@codeon.de>
2017-10-06 14:30:10 +0200

    update for 5.0.0.2

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-VERSION=5.0.0.1
-DIST=`lsb_release -sc`
+VERSION="5.0.0.2"
+DIST="`lsb_release -sc`"
+
 
 verify()
 {
@@ -12,7 +13,7 @@ verify()
       echo "cpack-mulle-clang has the wrong version" >&2
       exit 1
    fi
-  
+
    cd ..
 }
 
@@ -28,7 +29,7 @@ download()
 {
    cd mulle-clang-lldb
    curl -L -O "https://raw.githubusercontent.com/Codeon-GmbH/mulle-clang/mulle_objclang_50/install-mulle-clang.sh"
-   chmod 755 install-mulle-clang.sh 
+   chmod 755 install-mulle-clang.sh
    cd ..
 }
 
@@ -45,7 +46,7 @@ build()
       *)
          ./install-mulle-clang.sh --prefix `pwd`/opt/mulle-clang/${VERSION} --with-lldb
       ;;
-   esac 
+   esac
    cd ..
 }
 
@@ -55,15 +56,15 @@ verpack()
    cp cpack-mulle-clang/* mulle-clang-lldb/
 
    cd mulle-clang-lldb
-   chmod 755 generate-package.sh 
-   ./generate-package.sh 
+   chmod 755 generate-package.sh
+   ./generate-package.sh
    cd ..
 }
 
 upload()
 {
    scp mulle-clang-lldb/package/mulle-clang-${VERSION}-Linux.deb oswald:debian-software/dists/${DIST}/main/binary-amd64/mulle-clang-${VERSION}-${DIST}-amd64.deb
-} 
+}
 
 
 main()
@@ -75,7 +76,6 @@ main()
    done
 }
 
-set -x
 
 if [ $# -eq 0 ]
 then
